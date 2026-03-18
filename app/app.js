a04569ecf2280b62a9c565f1c6ce07069c0e13d8
Santhosh-git26 <srinivasan.mail1965@gmail.com>
2026-03-18 17:29:31 +0000

    new file update

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
-const http = require('http');
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.end("Hello from DevOps Project 🚀");
+app.get("/", (req, res) => {
+  res.send("Hello from DevOps CI/CD 🚀 (Demo Branch)");
 });
 
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
